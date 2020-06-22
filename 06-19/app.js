68a4165e3056e6487fae159f0fc03756c2552408
Katarzyna Mazur <mazur66@hotmail.com>
2020-06-21 23:42:43 -0400

    Changing the webiste styles with jQuery

--- 06-19/app.js
+++ 06-19/app.js
@@ -1,7 +1,16 @@
 
-
-$("#circle").click(function(){
+//Changing the paragraph text when the circle is clicked
+/*$("#circle").click(function(){
     $("p").html("This text has changed");
     
 
+})*/
+//changing the background color of the circle when the circle is clicked
+/*$("#circle").click(function(){
+    $("#circle").css("width", "400px");
+})*/
+//writing a function which will make any of the 3 shapes disappear when clicked
+
+$("div").click(function(){
+    $("div").css("display", "none");
 })
