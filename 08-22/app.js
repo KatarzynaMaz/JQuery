d4eafe1bf00059f41bfb958be1236bb3b3ad3ee3
Katarzyna Mazur <mazur66@hotmail.com>
2021-08-22 23:01:03 -0400

    Practiced JQuery - changing webiste content and style with css, fading elements when clicked

--- 08-22/app.js
+++ 08-22/app.js
@@ -3,3 +3,26 @@
 $("#circle").click(function(){
     $("p").html("The circle was clicked.");
 })
+
+/*alret "hello" when there is a hover on the circle
+    $("#circle").hover(function(){
+      alert("hello");
+    })*/
+
+//get the width of the body of the page
+//alert($("body").css("width"))
+
+/*click on a shape and make them all disseaper
+$("div").click(function(){
+    $("div").css("display", "none");
+})*/
+
+/*click on a shape and that particular shape diseaper
+$("div").click(function(){
+    $(this).css("display", "none");
+})*/
+
+//click on a shape and that particular shape fade out
+$("div").click(function(){
+    $(this).fadeOut();
+})
